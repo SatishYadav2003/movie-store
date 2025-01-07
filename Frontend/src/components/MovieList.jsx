de94d93ecb4e16540ef6c1834cf623175e549caa
SatishYadav2003 <sy389698@gmail.com>
2025-01-07 16:46:09 +0530

    loader added

--- Frontend/src/components/MovieList.jsx
+++ Frontend/src/components/MovieList.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MovieTemplate from "./MovieTemplate";
 import { useMovieResult } from "../contextApi/MovieProvider";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import ClipLoader from "react-spinners/ClipLoader"; // Import spinner
 
 function MovieList() {
   const [, movies] = useMovieResult();
-
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
   const handleMovieDetail = (movieJson) => {
@@ -15,11 +15,39 @@ function MovieList() {
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+
+    const preloadImages = async () => {
+      const imagePromises = movies.map(
+        (movie) =>
+          new Promise((resolve, reject) => {
+            const img = new Image();
+            img.src = movie["movie_img_url"];
+            img.onload = resolve;
+            img.onerror = reject;
+          })
+      );
+
+      try {
+        await Promise.all(imagePromises);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+      }
+    };
+
+    preloadImages();
+  }, [movies]);
 
   return (
     <div>
-      {movies.length === 0 ? (
+      {loading ? (
+        <div className="flex flex-col items-center">
+          <ClipLoader color="#3498db" loading={loading} size={80} />
+          <p className="text-center text-gray-500 text-xl mt-4">
+            Just a sec, movie magic coming your way!
+          </p>
+        </div>
+      ) : movies.length === 0 ? (
         <div className=" text-center text-red-500 font-semibold text-xl flex justify-center">
           <img
             src="https://cdni.iconscout.com/illustration/premium/thumb/no-search-found-2511608-2133696.png"
